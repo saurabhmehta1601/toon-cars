8d6465bcfe2f4f91cbc142d0bd8daf2291f8d5a5
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-07-09 18:10:02 +0530

    feat: add C5 model

---
+++ src/pages/3D/C5.tsx
@@ -0,0 +1,13 @@
+import { CanvasLayout } from "layouts/CanvasLayout";
+import React from "react";
+import C5Scene from "Scenes/C5Scene";
+
+const C5 = () => {
+  return (
+    <CanvasLayout>
+      <C5Scene />
+    </CanvasLayout>
+  );
+};
+
+export default C5;
